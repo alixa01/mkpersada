0f7bd10ebf647fc7925dcabd479ee02c0e1e371e
unknown <alixatofer07@gmail.com>
2025-11-20 15:32:54 +0700

    feat(our-project): add our project section

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import HeroSection from "./components/HeroSection";
 import AboutUsSection from "./components/AboutUsSection/AboutUsSection";
 import VisionMissionSection from "./components/VisionMissionSection/VisionMissionSection";
+import OurProjectSection from "./components/OurProjectSection";
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
       <HeroSection />
       <AboutUsSection />
       <VisionMissionSection />
+      <OurProjectSection />
     </>
   );
 }
