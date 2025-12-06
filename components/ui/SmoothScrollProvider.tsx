b8da0ba8b99b5b1de46f668ae3c84f267eaa6b92
unknown <alixatofer07@gmail.com>
2025-12-06 13:34:37 +0700

    update lenis, exc mobl

--- components/ui/SmoothScrollProvider.tsx
+++ components/ui/SmoothScrollProvider.tsx
@@ -11,21 +11,40 @@ declare global {
 
 export default function SmoothScrollProvider() {
   useEffect(() => {
+    const isMobile =
+      window.innerWidth < 768 ||
+      /Android|iPhone|iPad|iPod/i.test(navigator.userAgent);
+
+    if (isMobile) {
+      if (window.lenis) {
+        window.lenis.destroy();
+        window.lenis = undefined;
+      }
+
+      document.documentElement.style.scrollBehavior = "auto";
+      document.body.style.overflow = "auto";
+
+      return;
+    }
+
     const lenis = new Lenis({
       duration: 1.2,
-      easing: (t) => 1 - Math.pow(1 - t, 3),
+      easing: (t: number) => 1 - Math.pow(1 - t, 3),
     });
 
     window.lenis = lenis;
 
+    let frameId: number;
+
     function raf(time: number) {
       lenis.raf(time);
-      requestAnimationFrame(raf);
+      frameId = requestAnimationFrame(raf);
     }
 
-    requestAnimationFrame(raf);
+    frameId = requestAnimationFrame(raf);
 
     return () => {
+      cancelAnimationFrame(frameId);
       lenis.destroy();
       window.lenis = undefined;
     };
